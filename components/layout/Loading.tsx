a4696a7742f71fd057f856778109307699924b81
Noah <asde29873012549@gmail.com>
2024-05-05 19:05:46 +0800

    style: 💄 modify loading spinner style

--- components/layout/Loading.tsx
+++ components/layout/Loading.tsx
@@ -11,7 +11,9 @@ export default function Loading() {
 					: "hidden"
 			}
 		>
-			<div className="absolute h-12 w-12 animate-spin rounded-md border-[5px] border-t-4 border-cyan-600"></div>
+			<div className="text-surface inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white">
+				<span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]" />
+			</div>
 		</div>
 	);
 }
